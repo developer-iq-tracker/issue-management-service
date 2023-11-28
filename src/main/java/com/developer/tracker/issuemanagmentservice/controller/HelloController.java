185f562abe501fb74cf97e0f17de026c94f84b3e
senanayake <janith@edhirya.com>
2023-11-29 01:55:22 +0530

    add first api

---
+++ src/main/java/com/developer/tracker/issuemanagmentservice/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.developer.tracker.issuemanagmentservice.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping("/")
+    public ResponseEntity<String> sayHello() {
+        return ResponseEntity.ok("hello");
+    }
+}
