d25c4b91f268be7c29089b896990112e0b64eac7
senanayake <janith@edhirya.com>
2023-11-28 23:27:39 +0530

    initial commit

---
+++ src/test/java/com/developer/tracker/issuemanagmentservice/IssueManagmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developer.tracker.issuemanagmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueManagmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
