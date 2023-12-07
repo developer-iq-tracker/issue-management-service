a79951e21fbb581d2171c58cfa3ea85f6d13e123
senanayake <janith@edhirya.com>
2023-12-07 16:05:22 +0530

    update api

--- src/main/java/com/developer/tracker/issuemanagmentservice/controller/HelloController.java
+++ src/main/java/com/developer/tracker/issuemanagmentservice/controller/HelloController.java
@@ -5,12 +5,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
 
     @GetMapping("/")
-    public ResponseEntity<String> sayHello() {
-        return ResponseEntity.ok("hello hi hi");
+    public ResponseEntity<Map<String, String>> sayHello() {
+        Map<String, String> map = new HashMap<>();
+        map.put("response", "sucess");
+        return ResponseEntity.ok(map);
     }
 }
